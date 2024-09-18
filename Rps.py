775b1ad7e4dc164902aad4ed081e4702a5fe5ee7
jamiaw <s12145686@students.duvalschools.org>
2024-09-18 11:51:08 -0400

    Create Rps.py
    ROCK, PAPER, SISSORS

---
+++ Rps.py
@@ -0,0 +1,13 @@
+# Rock, Paper Scissors by Jamia Washington
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCTURES -- PLAYER
+playerscore = 0
+Playchoice = None
+Playername = "Test Player"
+
+# DATA STRUCTURES -- CPU
+cpuScore = 0
+cpuChoice = None 
